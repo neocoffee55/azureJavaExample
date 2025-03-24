8d8ff2b9134b5957dafe0c6f8a603b4616c442d0
Insu.Han <neocoffee55@gmail.com>
2025-03-24 14:37:01 +0900

    .

---
+++ src/main/java/com/example/azurespringbootapp/AzureSpringBootAppApplication.java
@@ -0,0 +1,23 @@
+package com.example.azurespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AzureSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureSpringBootAppApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Azure App Service!";
+    }
+}
